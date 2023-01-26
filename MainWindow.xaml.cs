9d910b22fea77b90034b0f165b4a3bd28edf0555
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-01-25 19:15:04 -0800

    Updated Notes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,25 +23,24 @@ namespace Prog_122_W23_Lecture_6_Class_Methods
     /// </summary>
     public partial class MainWindow : Window
     {
-    //        What is a method?
-    //What is the purpose of a method?
-    //What is a class method?
-    //Review: What’s the name for any field, constructor, property or method that is part of a class?
-    //Review: What are the 4 parts of declaring a method?
-    //What keyword allows you to access members related to the specific instance?
-    //A method build inside a class has access to what, even it’s access modifier is set to private?
-    //What does “override .ToString()” allow us to do?
-    //You use the* ____ operator * do you use to access members inside of an instanced class?
- 
+     
 
-    //Optional: 
+        //Review: What’s the name for any field, constructor, property or method that is part of a class?
+        //Review: What are the 4 parts of declaring a method?
+        //What keyword allows you to access members related to the specific instance?
+        //A method build inside a class has access to what, even it’s access modifier is set to private?
+        //What does “override .ToString()” allow us to do?
+        //You use the* ____ operator * do you use to access members inside of an instanced class?
 
-    //What is method overloading?
-    //What needs to be different to overload a method?
+
+        //Optional: 
+
+        //What is method overloading?
+        //What needs to be different to overload a method?
         public MainWindow()
         {
             InitializeComponent();
-          
+
         }
     }
 }
---
+++ Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_122_W23_Lecture_6_Class_Methods
+{
+    // internal
+    // public
+    // private
+    // protected
+    class Student
+    {
+        // Field
+        string _firstName;
+        string _lastName;
+        double _csiGrade;
+        double _genEdGrade;
+
+        // Constructor
+        //public Student(string firstName)
+        //{
+        //    this._firstName = firstName;
+        //}
+        public Student(string firstName, string lastName, double csiGrade, double genEdGrade)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _csiGrade = csiGrade;
+            _genEdGrade = genEdGrade;
+        }
+
+
+        // Properties
+        //    string _firstName;
+        public string FirstName
+        {
+            // get and set
+            // allows the user to read the value
+            // allows the user to change the value
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+        public string LastName { get => _lastName; set => _lastName = value; }
+        public double CsiGrade { get => _csiGrade; set => _csiGrade = value; }
+        public double GenEdGrade { get => _genEdGrade; set => _genEdGrade = value; }
+
+
+
+        // Method
+
+        // What is a method?
+        // A block of code that is given a name, and runs when it is called
+
+        //What is the purpose of a method?
+        // Easy to code, easy to read, easy to refactor, and easy to resuse
+
+        //What is a class method?
+
+    }
+}
